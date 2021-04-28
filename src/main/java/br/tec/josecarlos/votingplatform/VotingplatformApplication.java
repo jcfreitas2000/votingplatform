71634f872721f6e893799a9ca4eef66959d68bd0
José Carlos de Freitas <jcfreitas2000@hotmail.com>
2021-04-28 20:12:21 -0300

    initial project created by start.spring.io

---
+++ src/main/java/br/tec/josecarlos/votingplatform/VotingplatformApplication.java
@@ -0,0 +1,13 @@
+package br.tec.josecarlos.votingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/tec/josecarlos/votingplatform/VotingplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package br.tec.josecarlos.votingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
