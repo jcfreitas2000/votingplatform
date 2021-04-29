8ee2d9af7dc2fb3cfeb94011d18d181d6947642c
José Carlos de Freitas <jcfreitas2000@hotmail.com>
2021-04-28 22:25:28 -0300

    agenda name changed from nullable to non nullable

--- src/main/java/br/tec/josecarlos/votingplatform/models/Agenda.java
+++ src/main/java/br/tec/josecarlos/votingplatform/models/Agenda.java
@@ -3,6 +3,7 @@ package br.tec.josecarlos.votingplatform.models;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.UUID;
@@ -15,5 +16,6 @@ public class Agenda {
     @Id
     private UUID id = UUID.randomUUID();
 
+    @Column(nullable = false)
     private String name;
 }
